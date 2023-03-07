90a27c7fdae5293f0cb7a0b32ef084430c5a63bc
Chrism982 <chrism9822@gmail.com>
2023-03-07 18:12:37 +0100

    updated js script

--- js.js
+++ js.js
@@ -1,57 +1,54 @@
-const conventer = document.querySelector('#converter')
-const result = document.querySelector('.result')
-const convBtn = document.querySelector('.conv')
-const resetBtn = document.querySelector('.reset')
-const changeBtn = document.querySelector('.change')
-const one = document.querySelector('.one')
-const two = document.querySelector('.two')
+const converter = document.querySelector('#converter');
+const result = document.querySelector('.result');
+const convBtn = document.querySelector('.conv');
+const resetBtn = document.querySelector('.reset');
+const changeBtn = document.querySelector('.change');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
 
-
-let farenheit
-let celcius
+let farenheit;
+let celsius;
 
 const swap = () => {
-    if (one.textContent === '°C') {
-        one.textContent = '°F'
-        two.textContent = '°C'
-        result.textContent = ''
-    } else {
-        one.textContent = '°C'
-        two.textContent = '°F'
-        result.textContent = ''
-    }
-}
-
+	if (one.textContent === '°C') {
+		one.textContent = '°F';
+		two.textContent = '°C';
+		result.textContent = '';
+	} else {
+		one.textContent = '°C';
+		two.textContent = '°F';
+		result.textContent = '';
+	}
+};
 const fahrToCel = () => {
-    farenheit = converter.value * 1.8 + 32
-    result.textContent = `${conventer.value} °C to ${farenheit.toFixed(1)} °F`
-    converter.value = '' 
-}
+	farenheit = converter.value * 1.8 + 32;
+	result.textContent = `${converter.value} ℃ to ${farenheit.toFixed(1)} ℉`;
+	converter.value = '';
+};
 
-const celToFar = () => {
-    celcius = (converter.value - 32) / 1.8
-    result.textContent = `${converter.value} °F to ${celcius.toFixed(1)} °C`
-    converter.value = ''
-}  
+const celToFahr = () => {
+	celsius = (converter.value - 32) / 1.8;
+	result.textContent = `${converter.value} ℉ to ${celsius.toFixed(1)} ℃`;
+	converter.value = '';
+};
 
 const conversion = () => {
-    if (conventer.value !== ''){
-        if (one.textContent === '°C') {
-            fahrToCel()
-        } else {
-            celToFar()
-        }
-    } else {
-        result.textContent = 'Musisz podać jakąś wartość'
-    }
-}
+	if (converter.value !== '') {
+		if (one.textContent === '°C') {
+			fahrToCel();
+		} else {
+			celToFahr();
+		}
+	} else {
+		result.textContent = 'You must enter some value!';
+	}
+};
 
 const reset = () => {
-    conventer.value = ''
-    result.textContent = ''
-}
+	converter.value = '';
+	result.textContent = '';
+};
 
-
-changeBtn.addEventListener('click', swap)
-convBtn.addEventListener('click', conversion)
-resetBtn.addEventListener('click', reset)
+changeBtn.addEventListener('click', swap);
+convBtn.addEventListener('click', conversion);
+resetBtn.addEventListener('click', reset);
